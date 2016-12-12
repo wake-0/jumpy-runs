15100ff3ab4f626b892611fa517d3a099f18f8aa
Rittel <felix.rittel@gmail.com>
2016-12-12 11:38:03 +0100

    Fixed some jumping interactions (jump height was calculated dependant on current floor-height and now is dependant on start-height) NEED TO FIX clipping while jumping with an obstacle and being moved above it

--- lib/entities/player/playerPhysic.rb
+++ lib/entities/player/playerPhysic.rb
@@ -3,7 +3,7 @@ require './lib/architecture/position'
 require './lib/architecture/input'
 
 class PlayerPhysic < PhysicalComponent
-
+    
     def initialize(position, input_x, input_y, player_graphic)
         super(position, input_x, input_y, player_graphic)
         @gravity = 8
@@ -12,7 +12,7 @@ class PlayerPhysic < PhysicalComponent
         @is_jumping = false
         @highest_point_reached = false
     end
-
+    
     def update
         delta_x = 0
         delta_y = 0
@@ -24,34 +24,42 @@ class PlayerPhysic < PhysicalComponent
         elsif @input_x.direction == :none
             delta_x = 0
         end
-
+        
         if @input_y.direction == :top
+            if !@is_jumping
+                @jump_start = @position.y + @graphical_component.rectangle.height
+            end
             @is_jumping = true
             delta_y = -@input_y.delta * @jump_speed unless @highest_point_reached
+        else
+            @is_jumping = false
         end
+        
         # Do nothing when down is pressed
         # @position.updateDelta(0, @input.delta) if @input.direction == :down
-
+        
         delta_y = delta_y + gravity_delta
         @position.update_delta(delta_x, delta_y)
     end
-
+    
     private
     def distance_to_ground
         ground_y = $map.get_ground(@graphical_component.rectangle)
         player_y = @position.y + @graphical_component.rectangle.height
         ground_y - player_y
     end
-
+    
     def gravity_delta
         gravity_delta = 0
         gravity_delta = distance_to_ground if @highest_point_reached && distance_to_ground <= @gravity
         gravity_delta = @gravity if @highest_point_reached && distance_to_ground > @gravity
-
+        
         # This is used to check if the highest point while jumping was reached
         @highest_point_reached = false if distance_to_ground == 0
-        @highest_point_reached = true if distance_to_ground >= @jump_height || !@is_jumping
-        @is_jumping = false
+        @highest_point_reached = true if !@is_jumping || (@jump_start - (@position.y + @graphical_component.rectangle.height)) >= @jump_height
+        if @highest_point_reached
+            @is_jumping = false
+        end
         gravity_delta
     end
 
